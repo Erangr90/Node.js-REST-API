714fcc3e699dd2cbc9a7b83e7a4f275afa04db42
Eran Grady <62970558+Erangr90@users.noreply.github.com>
2023-06-09 19:51:28 +0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -7,7 +7,6 @@ import userRoutes from "./routes/userRoutes.js";
 // import uploadRoutes from './routes/uploadRoutes.js';
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import logger, { systemLogStream } from "./middleware/loggerMiddleware.js";
-// import { requestDecryption, responseEncryption } from './middleware/encryptionMiddleware.js';
 
 const port = process.env.PORT || 5000;
 
@@ -21,8 +20,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(logger);
-// app.use(requestDecryption);
-// app.use(responseEncryption);
 
 
 
